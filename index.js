ff1f81d61a40b2538d853b9182deb2b08cb45d41
Blackbottt <94114870+Blackbottt@users.noreply.github.com>
2022-08-26 12:47:18 +0200

    index.js update

--- index.js
+++ index.js
@@ -25,28 +25,32 @@ app.get("/api/timestamp/", function (req, res) {
 
   let date = new Date();
   
-  return res.json({
+  res.json({
     "unix": date.getTime(), 
     "utc": date.toUTCString()
   });
 });
 
 // your first API endpoint... 
+
 app.get("/api/timestamp/:date_str", function (req, res) {
   const { date_str } = req.params;
 
   let date = new Date(date_str);
 
-  if(date === 'Invalid Date') {
+  if(date.toString() === 'Invalid Date') {
+    date = new Date(parseInt(date_str));
+  }
+  
+  if(date.toString() === 'Invalid Date') {
     return res.json({ "error": "Invalid Date" });
   } else {
-    return res.json({
+   return res.json({
       "unix": date.getTime(), 
       "utc": date.toUTCString()
     });
   }
 });
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
